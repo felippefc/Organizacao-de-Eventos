ea743cd96b99670e9fc97f95755a157ae8bd8c03
felippefc <felippex1@hotmai.com>
2026-01-27 21:24:54 -0300

    feat: list events with loading and error states style: improve event list layout for large screens
    Signed-off-by: felippefc <felippex1@hotmai.com>

--- src/screens/eventListScreen.tsx
+++ src/screens/eventListScreen.tsx
@@ -1,3 +1,107 @@
+import { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
+import { getEvents } from '../api/apiEvents';
+import { Event } from '../types/events';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
 export function EventListScreen() {
-  return null;
+  const [events, setEvents] = useState<Event[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  async function loadEvents() {
+    try {
+      setLoading(true);
+      setError(null);
+      const data = await getEvents();
+      setEvents(data);
+    } catch {
+      setError('Erro ao carregar eventos');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadEvents();
+  }, []);
+
+  if (loading) {
+    return (
+      <SafeAreaView style={styles.center}>
+        <ActivityIndicator size="large" />
+      </SafeAreaView>
+    );
+  }
+
+  if (error) {
+    return (
+      <SafeAreaView style={styles.center}>
+        <Text>{error}</Text>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <Text style={styles.header}>Eventos</Text>
+
+      <FlatList
+        data={events}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.listContent}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text>{item.location}</Text>
+            <Text style={styles.status}>Status: {item.status}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
 }
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  listContent: {
+    paddingBottom: 24,
+  },
+  card: {
+    padding: 16,
+    borderRadius: 12,
+    backgroundColor: '#f2f2f2',
+    marginBottom: 16,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  status: {
+    marginTop: 4,
+    opacity: 0.7,
+  },
+});
+
